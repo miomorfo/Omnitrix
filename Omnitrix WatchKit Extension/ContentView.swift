df26798c24a0a2f74095d95acd91d8e903331eca
Fernando <fernando.eb@icloud.com>
2022-01-08 10:59:00 -0300

    Initial Commit

---
+++ Omnitrix WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Omnitrix WatchKit Extension
+//
+//  Created by Fernando on 08-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Omnitrix WatchKit Extension/OmnitrixApp.swift
@@ -0,0 +1,19 @@
+//
+//  OmnitrixApp.swift
+//  Omnitrix WatchKit Extension
+//
+//  Created by Fernando on 08-01-22.
+//
+
+import SwiftUI
+
+@main
+struct OmnitrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
